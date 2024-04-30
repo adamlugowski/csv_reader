cf2747d0c850ebb8044425407e81c702c7d6ae8e
Adam Ługowski <adam.lugowski@live.com>
2024-04-30 12:28:34 +0200

    moved code for making data to fake_data.py, introduce csv reader in main.py

---
+++ fake_data.py
@@ -0,0 +1,17 @@
+from faker import Faker
+import csv
+
+
+def make_fake_data(number_of_rows: int):
+    fake = Faker()
+    with open('names.csv', mode='w', newline='') as csvfile:
+        fieldnames = ['first name', 'last name', 'date of birth']
+        writer = csv.DictWriter(csvfile, delimiter=',', fieldnames=fieldnames)
+        writer.writeheader()
+        for _ in range(number_of_rows):
+            writer.writerow({'first name': fake.first_name(),
+                             'last name': fake.last_name(),
+                             'date of birth': fake.date_of_birth()})
+
+
+make_fake_data(100)
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-from faker import Faker
-from datetime import date
 import csv
 
-fake = Faker()
 
-with open('names.csv', mode='w') as csvfile:
-    fieldnames = ['first name', 'last name', 'date of birth']
-    writer = csv.DictWriter(csvfile, delimiter=',', fieldnames=fieldnames)
-    writer.writeheader()
-    for _ in range(3):
-        writer.writerow({'first name': fake.first_name(),
-                         'last name': fake.last_name(),
-                         'date of birth': fake.date_of_birth()})
+with open('names.csv', mode='r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print('Imię:', row['first name'], '\n'
+              'Nazwisko:', row['last name'], '\n'
+              'Data urodzenia:', row['date of birth'], '\n')
